43cd1d0e49b5b9f0331887b55c5483bf211d1dfa
arunabhdas <arunabhdas@gmail.com>
2023-11-10 03:33:59 -0500

    Added JSON function calling

--- app/router/openai.py
+++ app/router/openai.py
@@ -15,18 +15,27 @@ router = APIRouter(
 ###############################################################################
 ## OpenAI
 
-@router.post('/openai/vision')
-def dovision(
+@router.get('/openai/functioncalling')
+def dofunctioncalling(
     *,
     session: Session = Depends(database.get_session),
     current_user: models.User = Depends(oauth2.get_current_user),
+    query: str = Query(..., description="The content to send to the OpenAI model")
 ):
+    client = openai.OpenAI()
     response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
+        model="gpt-4-1106-preview",
         messages = [
             {
-                "role": "user"
+                "role": "system",
+                "content": "Generate JSON response"   
+            },
+            {
+                "role": "user",
+                "content": query
                 
             }
-        ]
+        ],
+        response_format={ "type": "json_object" }
     )
+    return response.choices[0].message.content
